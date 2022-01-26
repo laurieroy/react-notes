a08b6c6dd9ef62d6f5c918b930eba19f1fb17677
lauretteroy <laurieroy.dev@gmail.com>
2022-01-26 11:47:58 -0800

    Fix nav

--- src/components/App.js
+++ src/components/App.js
@@ -5,17 +5,18 @@ import Auth from "../pages/Auth.jsx";
 import Dashboard from "../pages/Dashboard.jsx";
 import Home from "../pages/Home.jsx";
 import Nav from "./Nav.jsx";
+import { useAppState } from "../AppState.jsx";
 
 export const App = (props) => {
   return (
     <>
-      <Nav>
+      <Nav />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/auth/:form" component={Auth} />
           <Route path="/dashboard" component={Dashboard} />
         </Switch>
-      </Nav>
+  
     </>
   );
 };
